74fac3015dabe72c30534109795a362728fb9987
tuanhung2005 <BIT230186@st.cmc-u.edu.vn>
2025-06-12 12:03:59 +0700

    noi dung

---
+++ src/main/java/com/mycompany/no01/No01.java
@@ -0,0 +1,28 @@
+package com.mycompany.no01;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class No01 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Product Table");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            String[] columns = {"ID", "Name", "Price"};
+            Object[][] data = {
+                {"1", "Apple", 10},
+                {"2", "Banana", 5},
+                {"3", "Orange", 7},
+                {"4", "Grapes", 12}
+            };
+
+            JTable table = new JTable(new DefaultTableModel(data, columns));
+            JScrollPane scrollPane = new JScrollPane(table);
+            frame.add(scrollPane);
+            frame.setSize(400, 300);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+}
